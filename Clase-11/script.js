778fca1054c4c4ef5602b2aed6b14c2a69b83723
wperaltaarriaga <wperaltaarriaga@gmail.com>
2024-05-08 19:30:33 -0300

    Clase 11

---
+++ Clase-11/script.js
@@ -0,0 +1,31 @@
+/* TIPOS DE DATOS */
+
+/* Datos primitivos:
+-Number (numeros): 8, 0, 2, 3.14 (es numero float), NaN, Infinity.
+-String (textos): "hola", 'hola', ´hola´.
+-Boolean: true, false.
+-Null (ausencia o nulidad de un dato): null.
+-Undefined (no hay un dato definido): undefined.
+*/
+
+/* Falsy o Truthy
+si al pasarlo por Boolean(dato) me da false, es un valor FALSY
+y si al pasarlo me da true, es un valor TRUTHY
+*/
+
+/* COMPARADORES:
+nos permiten comparar valores. Siempre devuelven booleanos
+Nan no puede ser comparado con ningun otro dato
+
+
+- == es igual un dato a otro. Compara el VALOR
+ej: 1==1 ==> true, 1=='1' ==> true, '1'== true ==> true, null==0 => false
+
+-=== estricta igualdad. Compara VALOR y TIPO DE DATO
+ej: 
+
+-!= diferencia
+
+-!== estricta diferencia
+
+*/
