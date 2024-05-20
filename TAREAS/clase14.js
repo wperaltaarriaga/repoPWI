b6af6376a844a84fb86384dc65524629d2e098ed
wperaltaarriaga <wperaltaarriaga@gmail.com>
2024-05-20 19:26:48 -0300

    clase 14

---
+++ TAREAS/clase14.js
@@ -0,0 +1,17 @@
+/* ARRAYS */
+
+let producto{
+    precio: 500,
+    nombre: 'anteojos',
+    descripcion: 'anteojos de sol',
+    marca: 'infinity',
+}
+le
+let persona{
+    nombre: 'pepe',
+    edad: 23,
+    mejoramigo: {
+        nombre: 'milagros',
+        edad: 22,
+    }
+}
